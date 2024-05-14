85b21a7e7feaf7a4a9816138bb939e24316ab5b1
DanieleBegnis <daniele.begnis@gmail.com>
2024-05-14 16:00:43 +0200

    feat: add class, function and constructor

--- index.php
+++ index.php
@@ -4,3 +4,25 @@
    => all'interno della classe è definito un costruttore
    => all'interno della classe è definito almeno un metodo
 - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
+
+<?php 
+// classe movie
+class Movie {
+    public $name;
+    public $plot;
+    public $director;
+
+    //costruttore
+
+    public function __construct($_name, $_plot, $_director) {
+        $this->name = $_name;
+        $this->plot = $_plot;
+        $this->director = $_director;
+    }
+
+    public function getMovieDirector() {
+        return $this->name . ' di ' . $this->director;
+    }
+}
+
+?>
